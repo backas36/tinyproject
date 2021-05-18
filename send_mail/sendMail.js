5893497d4f716609e5470a2f7b9f8cc3bd444cb8
Ashi <backas36@gmail.com>
2021-05-19 01:48:20 +0800

    增加一次寄信給很多人的功能

--- send_mail/sendMail.js
+++ send_mail/sendMail.js
@@ -1,8 +1,42 @@
 const mailgun = require("mailgun-js");
+const fs = require('fs');
 const DOMAIN = 'sandboxd4a40f661f4147b08c1dbee345c0b581.mailgun.org';
 const api_key = '639b31af330cdeeb231aa262ca9125ae-6ae2ecad-1c64744e'
 const mg = mailgun({apiKey:api_key, domain: DOMAIN});
 
+const data = fs.readFileSync('./mail_list.txt','utf8')
+const emails = data.split('\n')
+  .filter(email=> email !=='')
+  .map(email=>({
+    to:email.split(' ')[0],
+    name:email.split(' ')[1]
+  }))
+  //如果沒有用 ()包起來，js 不能理解我們要回傳的是 object
+
+const subjectTemplate = `Long time no see, {{name}}`
+const messageTemplate = `
+  Hello, {{name}}
+  How are you today?
+`
+// 寄給 emails[index] 
+const sendEmailIndex = (index) => {
+  if (index >= emails.length) return 
+  // 如果 index 超過 emails 長度 就不執行
+  let {to,name} = emails[index]
+  console.log(`sending email to ${to} now ...`)
+  send(
+    to,
+    subjectTemplate.replace('{{name}}', name),
+    messageTemplate.replace('{{name}}', name),
+    function (err, body) {
+      if (err) {
+        return console.log('QQ', err)
+      }
+      console.log(body)
+      sendEmailIndex(index +1 )
+    }
+  )
+}
 
 const send = (to, subject, text, callback) => {
   const data = {
@@ -16,9 +50,4 @@ const send = (to, subject, text, callback) => {
   });
 }
 
-send('backas36+ashi@gmail.com','主旨','this is 內容', function(error,data){
-  if (error) {
-    return console.log(error)
-  }
-  console.log(data)
-})
+sendEmailIndex(0)
